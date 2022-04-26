a002c3123130b3e04b10a35897aadc369636b4ba
scottbing <bingscott@gmail.com>
2022-04-26 17:29:00 -0600

    BSSD5350_FinalQuestion1

--- question1/albers.glsl
+++ question1/albers.glsl
@@ -1,9 +1,16 @@
 /* Created by Brandon Woolworth on 6/18/2017 */
 
+#ifdef GL_ES
+ precision mediump float;
+ #endif
+
 /* Some simple precompilation stuff, so we don't have to recreate it later. */
 #define rgb(r,g,b) vec3(r/255., g/255., b/255.)
 #define desiredColor rgb(255., 0., 0.)
 #define burntOrange rgb(250., 75., 30.)
+#define goldenYellow rgb(255., 192., 0.)
+#define PROCESSING_COLOR_SHADER
+
 
 /**
  * Draw a rectangle
@@ -14,13 +21,13 @@
  * @param vec3 color   - Desired color of the rectangle
  */
  
- #ifdef GL_ES
- precision mediump float;
- #endif
- 
 uniform vec2 iResolution;
 uniform float time;
- 
+uniform vec3 backgroundColor;
+uniform vec3 rectOneColor;
+uniform vec3 rectTwoColor;
+uniform vec3 rectThreeColor;
+
 vec4 rectangle(vec2 uv, vec2 pos, float width, float height, vec3 color) {
   float t = 0.0;
     if ((uv.x > pos.x - width / 2.0) && (uv.x < pos.x + width / 2.0)
@@ -44,22 +51,37 @@ vec4 rectangle(vec2 uv, vec2 pos, float width, float height, vec3 color) {
  */
 void main()
 {
-  uv[1] = uv[1]*0.25
-  vec2 uv = gl_FragCoord.xy;
+    vec2 uv = gl_FragCoord.xy;
     
     // Create a rectangle at the center screen with width 2/4 the screen and size 2/4 screen
-    vec2 desiredPos = iResolution.xy * 0.5;
-    float width = iResolution.x  * 0.25;
+    vec2 rectOnePos = vec2(iResolution.x * 0.5, iResolution.y * 0.35);
+    float width = (iResolution.x  * 0.25) + 50;
     float height = width;
-    vec4 rectOne = rectangle(uv, desiredPos, width, height, desiredColor);
+    vec4 rectOne = rectangle(uv, rectOnePos, width, height, rectOneColor);
+
+    // Create an enclosing rectangle
+    //vec2 rectTwoPos = iResolution.xy * 0.5;
+    vec2 rectTwoPos = vec2(iResolution.x * 0.5, iResolution.y * 0.40);
+    width = (iResolution.x  * 0.25) + 100;
+    height = width;
+    vec4 rectTwo = rectangle(uv, rectTwoPos, width, height, rectTwoColor);
+    
+    // Create an enclosing rectangle
+    vec2 rectThreePos = vec2(iResolution.x * 0.5, iResolution.y * 0.45);
+    width = (iResolution.x  * 0.25) + 150;
+    height = width;
+    vec4 rectThree = rectangle(uv, rectThreePos, width, height, rectThreeColor);
     
     // Create a rectangle to show in the background of the screen
     vec2 fullScreen = vec2(0., 0.);
-    float fullWidth = iResolution.x;
-    float fullHeight = iResolution.y;
-    vec4 backgroundRect = rectangle(uv, fullScreen, fullWidth, fullHeight, burntOrange);
+    float fullWidth = iResolution.x*2;
+    float fullHeight = iResolution.y*2;
+    vec4 backgroundRect = rectangle(uv, fullScreen, fullWidth, fullHeight, backgroundColor);
     
     // Divide the colors accordingly.
     // Keeps the backgroundRect separate from rectOne, while also producing a color to set gl_FragColor to.
-  gl_FragColor = mix(backgroundRect, rectOne, rectOne.a);
+    gl_FragColor = mix(rectOne, rectTwo, rectTwo.a);
+    gl_FragColor += mix(backgroundRect, rectOne, rectOne.a);
+    gl_FragColor += mix(rectTwo, rectThree, rectThree.a);
+    
 }
--- question1/question1.pde
+++ question1/question1.pde
@@ -1,25 +1,19 @@
 PShader albers;
 
-HelperFunctions hf = new HelperFunctions();
-
 void setup() {
- size(640, 360, P2D); 
+ size(300, 300, P2D); 
  noStroke();
 
  albers = loadShader("albers.glsl"); 
  albers.set("iResolution", float(width), float(height));
- albers.set("time", millis() / 500.0);
-
+ noLoop();
 }
 
 void draw() {
- shader(albers);
- rect(0.0, 0.0, width, height);
- albers.set("time", millis() / 500.0);
-}
-
-void keyPressed() {
-  if (key==' ') {
-    hf.save("img");
-  }
+  albers.set("baseColor", random(255)/255.0, random(255)/255.0, random(255)/255.0); // color from 0 - 1
+  albers.set("backgroundColor", random(255)/255.0, random(255)/255.0, random(255)/255.0); // color from 0 - 1
+  albers.set("rectTwoColor", random(255)/255.0, random(255)/255.0, random(255)/255.0); // color from 0 - 1
+  albers.set("rectThreeColor", random(255)/255.0, random(255)/255.0, random(255)/255.0); // color from 0 - 1
+  shader(albers);
+  rect(0.0, 0.0, width, height);
 }
